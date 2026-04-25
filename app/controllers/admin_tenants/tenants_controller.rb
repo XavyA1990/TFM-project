3203e21391bde43773e08e82c87477743311f2fb
XavyA1990 <contrerasdeveloper@outlook.com>
2026-04-25 09:16:08 -0500

    feat: [TFM-23][Andres] Enhance tenant management features with detailed views and updates, including localization improvements

--- app/controllers/admin_tenants/tenants_controller.rb
+++ app/controllers/admin_tenants/tenants_controller.rb
@@ -1,10 +1,37 @@
 class AdminTenants::TenantsController < AdminTenants::BaseController
+  before_action :set_tenant
+
   def show
+    @tenant_details = AdminTenants::TenantsServices.new(:show, { tenant: @tenant }).call
+    @tenant_non_customer_memberships = Admin::MembershipsServices.new(
+      :for_tenant_without_customer,
+      { tenant: @tenant }
+    ).call
   end
 
   def edit
   end
 
   def update
+    @tenant = AdminTenants::TenantsServices.new(
+      :update,
+      { tenant: @tenant, attributes: tenant_params }
+    ).call
+
+    if @tenant.errors.any?
+      render :edit, status: :unprocessable_entity
+    else
+      redirect_to admin_tenants_tenant_path(tenant_slug: @tenant.slug)
+    end
+  end
+
+  private
+
+  def set_tenant
+    @tenant = AdminTenants::TenantsServices.new(:get, { tenant: current_tenant }).call
+  end
+
+  def tenant_params
+    params.require(:tenant).permit(:name, :description, :header_text, :subheader_text, :logo_url)
   end
 end
---
+++ app/services/admin_tenants/tenants_services.rb
@@ -0,0 +1,41 @@
+module AdminTenants
+  class TenantsServices
+    def initialize(action, params, repository: TenantsRepository)
+      @action = action
+      @params = params
+      @repository = repository
+      @tenant = params[:tenant]
+    end
+
+    def call
+      return get_tenant_for_show_page if @action == :show
+      return get_tenant if @action == :get
+      return update if @action == :update
+
+      raise ArgumentError, "Invalid action"
+    end
+
+    private
+
+    def get_tenant_for_show_page
+      [
+        [Tenant.human_attribute_name(:name), @tenant.name],
+        [Tenant.human_attribute_name(:slug), @tenant.slug],
+        [Tenant.human_attribute_name(:description), @tenant.description],
+        [Tenant.human_attribute_name(:header_text), @tenant.header_text],
+        [Tenant.human_attribute_name(:subheader_text), @tenant.subheader_text],
+        [Tenant.human_attribute_name(:logo_url), @tenant.logo_url],
+        [Tenant.human_attribute_name(:created_at), @tenant.created_at ? I18n.l(@tenant.created_at) : nil],
+        [Tenant.human_attribute_name(:updated_at), @tenant.updated_at ? I18n.l(@tenant.updated_at) : nil]
+      ]
+    end
+
+    def get_tenant
+      @tenant
+    end
+
+    def update
+      @repository.update(@tenant.id, @params[:attributes])
+    end
+  end
+end
